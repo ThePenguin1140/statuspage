9b91ade814d1d4e8ad191cb2c79eb2372b0f820a
Jason Wuertz <jwuertz@unb.ca>
2018-09-11 14:18:13 -0300

    feat(interfaces): add abstract item class

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -4,3 +4,29 @@ export enum TodoStatus {
     DOING,
     DONE
 }
+
+export abstract class Item {
+    private _content: string;
+    private _status: ItemStatus;
+
+    constructor( content: string ) {
+        this._content = content;
+        this._status = ItemStatus.BACKLOG;
+    }
+
+    getContent(): string {
+        return this._content;
+    };
+
+    getStatus(): ItemStatus {
+        return this._status;
+    }
+
+    setContent( content: string ): void {
+        this._content = content;
+    }
+
+    setStatus( status: ItemStatus ): void {
+        this._status = status;
+    }
+}
