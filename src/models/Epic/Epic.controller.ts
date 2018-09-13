abae57a1494637d1360c2461d67eaa6f943e4bf9
Jason Wuertz <jwuertz@unb.ca>
2018-09-13 07:48:22 -0300

    feat(epic.router): add routes and stubs into ctrl

--- src/models/Epic/Epic.controller.ts
+++ src/models/Epic/Epic.controller.ts
@@ -1,3 +1,14 @@
+import { Context, Middleware } from 'server/typings/common';
+
 export class EpicController {
 
+    static list: Middleware = async ( ctx: Context ) => {};
+
+    static item: Middleware = async ( ctx: Context ) => {};
+
+    static create: Middleware = async ( ctx: Context ) => {};
+
+    static update: Middleware = async ( ctx: Context ) => {};
+
+    static delete: Middleware = async ( ctx: Context ) => {};
 }
--- src/models/Epic/Epic.router.ts
+++ src/models/Epic/Epic.router.ts
@@ -1,8 +1,14 @@
 import { get, post, put, del } from 'server/router';
+import { Middlewares } from "server/typings/common";
 import { EpicController } from './Epic.controller';
+import { Epic } from "./Epic";
 
-var epicRoutes: any[] = [
-
+var epicRoutes: Middlewares = [
+    get( '/epic', EpicController.list),
+    get( '/epic:id', EpicController.item ),
+    post( '/epic', EpicController.create ),
+    put( '/epic:id', EpicController.update ),
+    del( '/epic:id', EpicController.delete )
 ];
 
 export default epicRoutes;
